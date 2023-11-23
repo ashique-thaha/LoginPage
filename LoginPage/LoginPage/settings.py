3ac0eff499661e079d8d8b5101444041935bfb86
Ashique Thaha <ashiquethahaofficial@gmail.com>
2023-11-23 20:02:33 +0530

    Update settings.py

--- LoginPage/LoginPage/settings.py
+++ LoginPage/LoginPage/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uooaj=p8h+10fdcn=)2a*5naukal17lq6&taq-olgq*pd(bcr7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://ashique-thaha.github.io/LoginPage/']
 
